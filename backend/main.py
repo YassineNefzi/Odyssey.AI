b919e8afb634e9dfbf6b6472500da150163fe17f
YassineNefzi <ynyassine7@gmail.com>
2025-10-21 13:40:09 +0100

    add: fastapi app and middleware

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+
+app = FastAPI(
+    title="Interactive Story Generator",
+    description="API to generate stories",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 
 if __name__ == "__main__":
-    main()
+
+    import uvicorn
+
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
