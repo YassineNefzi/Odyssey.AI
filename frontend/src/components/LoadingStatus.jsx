dd8a10aa6a02feb2826af6365c3e97168e302ba2
YassineNefzi <ynyassine7@gmail.com>
2025-10-22 10:09:24 +0100

    add: loading status

---
+++ frontend/src/components/LoadingStatus.jsx
@@ -0,0 +1,17 @@
+function LoadingStatus({theme}) {
+
+    return <div className="loading-container">
+        <h2>Generating your {theme} story</h2>
+
+        <div className="loading-animation">
+            <div className="spinner"></div>
+        </div>
+
+        <p className="loading-info">
+            Please wait while we create an exciting interactive story for you...
+        </p>
+    </div>
+
+}
+
+export default LoadingStatus;
